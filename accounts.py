2a967311cc46c7b262a56edf016749962d5b100b
Mariusz Perzynski <m.perzyk@interia.pl>
2021-03-28 13:53:40 +0200

    add all ele from main to function

--- accounts.py
+++ accounts.py
@@ -1,5 +1,8 @@
-print("WITAJ W BANKU MAN1EXTERA")
-print("=========================")
-print("[1] ====> ZALOGUJ <====")
-print("[2] ====> ZAREJESTRUJ <====")
-print("=========================")
+def menu1():
+ print("WITAJ W BANKU MAN1EXTERA")
+ print("=========================")
+ print("[1] ====> ZALOGUJ <====")
+ print("[2] ====> ZAREJESTRUJ <====")
+ print("=========================")
+
+menu1()
